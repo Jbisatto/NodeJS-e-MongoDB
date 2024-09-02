fd9cb04f60bd9ad0fd207379879d32a8d2e69a30
Jefferson Bisatto <jefferson.bisatto@gmail.com>
2024-09-02 15:56:57 -0300

    Criando um formulário e adcionando o registro no banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
 var connMySQL = function(){
-	console.log('Conexao com bd foi estabelecida');
-	return mysql.createConnection({
-		host : 'localhost',
-		user : 'root',
-		password : '1234',
-		database : 'portal_noticias'
-	});
+    console.log('Conexao com o bd foi estabelecida');
+    return connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+    });
 }
 
-module.exports = function () {
-	console.log('O autoload carregou o módulo de conexão com bd');
-	return connMySQL;
+module.exports = function(){
+    console.log('O autoload carregou o módulo de conexão com o bd');
+    return connMySQL;
 }
