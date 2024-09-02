a535b917666e3e1351a63fae088a947b17bc0c66
Jefferson Bisatto <jefferson.bisatto@gmail.com>
2024-09-02 15:09:58 -0300

    centralizando a conexão com banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,7 @@
+var dbConnection = require('../../config/dbConnection');
 module.exports = function(app){
     app.get('/noticias', function(req,res){
-
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'admin',
-            database: 'portal_noticias'
-        });
-
+        var connection = dbConnection();
         connection.query("SELECT * FROM noticias", function(error, result){
             res.render('noticias/noticias', { noticias : result });
         });
