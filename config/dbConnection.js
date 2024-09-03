dc67ab254f97bec9670d61fc8af417fa26e2983b
Jefferson Bisatto <jefferson.bisatto@gmail.com>
2024-09-02 21:32:50 -0300

    Criando controllers

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql');
 
 var connMySQL = function(){
-    return connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'portal_noticias'
-    });
+	return mysql.createConnection({
+		host : 'localhost',
+		user : 'root',
+		password : 'admin',
+		database : 'portal_noticias'
+	});
 }
 
-module.exports = function(){
-    return connMySQL;
+module.exports = function () {
+	return connMySQL;
 }
